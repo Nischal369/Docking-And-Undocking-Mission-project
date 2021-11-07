f26f2f4cfcd152186620d1d409dbc1432497e958
Nischal369 <87186339+Nischal369@users.noreply.github.com>
2021-11-07 15:18:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function draw() {
     // spacecraft = spacecraftx + random(-1,1);
     // spacecraft.x = spacecraft.x + Random(-1,1);
     // spacecraft.x = spacecraft.x + randomNumbers(-1,1);
-    // spacecraft.x = spacecraft.x + random(-1,1);
+     spacecraft.x = spacecraft.x + random(-1,1);
     
   if(keyDown("UP_ARROW")){
     spacecraft.y = spacecraft.y -2;
@@ -73,12 +73,12 @@ function draw() {
   //   text("Docking UnSuccessful!", 200, 300);
   // }
 
-  // if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
-  //   hasDocked = true;
-  //   textSize(25);
-  //   fill("white")
-  //   text("Docking Successful!", 200, 300);
-  // }
+   if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
+     hasDocked = true;
+     textSize(25);
+     fill("white")
+     text("Docking Successful!", 200, 300);
+   }
 
   drawSprites();
 }
